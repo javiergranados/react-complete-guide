e54b5c2f6f4b13418aa7c77f39050bec92e7a156
jvgranados <jgranadosflores@gmail.com>
2018-06-03 16:47:05 +0200

    Added state in App component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import './App.css'
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 27 },
+      { name: 'John', age: 32 },
+      { name: 'Samantha', age: 29 }
+    ]
+  }
+
   render () {
     return (
       <div className='App'>
         <h1>Hello world!</h1>
-        <Person name='Max' age='27' />
-        <Person name='John' age='32' />
-        <Person name='Samantha' age='29' />
+        {this.state.persons.map((person, i) => <Person key={i} name={person.name} age={person.age} />)}
       </div>
     )
   }
