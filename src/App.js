4d63f3e7b81f719bf1f6d794e4e0454517c2d299
jvgranados <jgranadosflores@gmail.com>
2018-06-25 03:03:41 +0200

    Implemented radium library

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
+import Radium, { StyleRoot } from 'radium'
 import Person from './Person/Person'
 
 class App extends Component {
@@ -45,28 +46,40 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: ''
+      }
     }
 
-    const buttonToogleStyle = {...buttonStyle}
-    buttonToogleStyle.backgroundColor = this.state.showPersons ? 'red' : 'green'
+    const buttonToogleStyle = JSON.parse(JSON.stringify(buttonStyle))
+
+    if (this.state.showPersons) {
+      buttonToogleStyle.backgroundColor = 'red'
+      buttonToogleStyle[':hover'].backgroundColor = 'salmon'
+    } else {
+      buttonToogleStyle.backgroundColor = 'green'
+      buttonToogleStyle[':hover'].backgroundColor = 'lightgreen'
+    }
 
     return (
-      <div className='App'>
-        <h1>React complete guide</h1>
-        <p>Person title will be yellow if his age is major than 45 and red if it's 50 or major</p>
-        <button style={buttonToogleStyle} className='toogleButton' onClick={this.tooglePersonsHandler}>
-          {this.state.showPersons ? 'Hide' : 'Show'} persons
-        </button>
-        { this.state.showPersons &&
-          <div>
-            <button style={buttonStyle} onClick={() => this.incrementAgeHandler()}>Increment age all persons</button>
-            {this.state.persons.map((person, i) => <Person key={person.id} id={person.id} name={person.name} age={person.age} click={this.incrementAgeHandler} index={i + 1} />)}
-          </div>
-        }
-      </div>
+      <StyleRoot>
+        <div className='App'>
+          <h1>React complete guide</h1>
+          <p>Person title will be yellow if his age is major than 45 and red if it's 50 or major</p>
+          <button key='1' style={buttonToogleStyle} className='toogleButton' onClick={this.tooglePersonsHandler}>
+            {this.state.showPersons ? 'Hide' : 'Show'} persons
+          </button>
+          { this.state.showPersons &&
+            <div>
+              <button style={buttonStyle} onClick={() => this.incrementAgeHandler()}>Increment age all persons</button>
+              {this.state.persons.map((person, i) => <Person key={person.id} id={person.id} name={person.name} age={person.age} click={this.incrementAgeHandler} index={i + 1} />)}
+            </div>
+          }
+        </div>
+      </StyleRoot>
     )
   }
 }
 
-export default App
+export default Radium(App)
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import './Person.css'
+import Radium from 'radium'
 
 const person = props => {
   const classes = []
   props.age > 45 && classes.push('yellow')
   props.age >= 50 && classes.push('red')
 
+  const style = {
+    '@media (min-width: 500px)': {
+      width: '450px'
+    }
+  }
+
   return (
-    <div className='Person'>
+    <div className='Person' style={style}>
       <p className={classes.join(' ')}>Person {props.index}</p>
       <p>I'm {props.name} and I'm <span className='age' onClick={() => props.click(props.id)}>{props.age}</span> years old</p>
     </div>
   )
 }
 
-export default person
+export default Radium(person)
