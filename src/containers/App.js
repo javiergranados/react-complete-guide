4c26da4c060d70734dd8c53429b41c5d2f789503
jvgranados <jgranadosflores@gmail.com>
2018-06-26 23:43:52 +0200

    Used setState correctly

--- src/containers/App.js
+++ src/containers/App.js
@@ -16,8 +16,10 @@ class App extends Component {
   }
 
   tooglePersonsHandler = () => {
-    this.setState({
-      showPersons: !this.state.showPersons
+    this.setState((prevState, props) => {
+      return {
+        showPersons: !prevState.showPersons
+      }
     })
   }
 
